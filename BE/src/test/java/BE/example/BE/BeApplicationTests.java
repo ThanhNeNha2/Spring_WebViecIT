98483058ba6a669047e6b2559b61a349c888048d
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2025-03-05 01:22:34 +0700

    first code

---
+++ BE/src/test/java/BE/example/BE/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package BE.example.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
