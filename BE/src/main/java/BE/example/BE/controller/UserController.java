a1e7f0f83543b9a76aa279e99ac26fca699fbc4f
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2025-03-05 17:00:13 +0700

    CRUD user

--- BE/src/main/java/BE/example/BE/controller/UserController.java
+++ BE/src/main/java/BE/example/BE/controller/UserController.java
@@ -1,14 +1,19 @@
 package BE.example.BE.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import BE.example.BE.domain.User;
 import BE.example.BE.service.UserService;
+import jakarta.websocket.server.PathParam;
 
 @RestController
 public class UserController {
@@ -19,16 +24,33 @@ public class UserController {
         this.userSevice = userSevice;
     }
 
+    // GET ALL
+    @GetMapping("/user")
+    public List<User> getAllUser() {
+        return this.userSevice.handleGetUser();
+    }
+
+    // GET BY ID
+    @GetMapping("/user/{id}")
+    public User getByIdUser(@PathVariable long id) {
+        return this.userSevice.handleGetByIdUser(id);
+    }
+
     // CREATE
     @PostMapping("/user")
     public User createNewUser(@RequestBody User userPostman) {
         return this.userSevice.handleCreateUser(userPostman);
     }
 
+    // UPDATE
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User userUpdatePostman) {
+        return this.userSevice.handleUpdateUser(userUpdatePostman);
+    }
+
     // DELETE
     @DeleteMapping("/user/{id}")
     public boolean deleteUser(@PathVariable long id) {
         return this.userSevice.handleDeleteUser(id);
-
     }
 }
--- BE/src/main/java/BE/example/BE/service/UserService.java
+++ BE/src/main/java/BE/example/BE/service/UserService.java
@@ -1,5 +1,8 @@
 package BE.example.BE.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import BE.example.BE.domain.User;
@@ -14,10 +17,45 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    // Create
     public User handleCreateUser(User user) {
         return this.userRepository.save(user);
     }
 
+    // Get
+    public List<User> handleGetUser() {
+        return this.userRepository.findAll();
+    }
+
+    // Get by id
+    // *** C1
+    // public Optional<User> handleGetByIdUser(long id) {
+    // return this.userRepository.findById(id);
+    // }
+    // *** C2
+    public User handleGetByIdUser(long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        return null;
+    }
+
+    // update
+    public User handleUpdateUser(User user) {
+
+        User existingUser = this.handleGetByIdUser(user.getId());
+
+        if (existingUser != null) {
+            existingUser.setName(user.getName()); // Cập nhật từng thuộc tính
+            existingUser.setEmail(user.getEmail());
+            existingUser.setPassword(user.getPassword());
+            existingUser = this.userRepository.save(existingUser); // Lưu vào DB
+        }
+        return existingUser;
+    }
+
+    // delete
     public boolean handleDeleteUser(long id) {
         if (this.userRepository.existsById(id)) {
             this.userRepository.deleteById(id);
